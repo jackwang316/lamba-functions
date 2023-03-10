ac2cc2200da211ee344b069de98e58bc93a4cbfc
Eric Reyes <43354881+ejrey@users.noreply.github.com>
2023-03-09 23:52:52 -0800

    Update getDeckByOwnerId.py
    Final

--- getDeckByOwnerId.py
+++ getDeckByOwnerId.py
@@ -1,5 +1,9 @@
 import boto3
-from boto3.dynamodb.conditions import Attr
+import json
+from boto3.dynamodb.conditions import Key, Attr
+
+#For Reference on scanning the table
+#https://dynobase.dev/dynamodb-python-with-boto3/#scan
 
 sts_client = boto3.client('sts')
 
@@ -7,20 +11,36 @@ def lambda_handler(event, context):
     sts_response = sts_client.assume_role(RoleArn='arn:aws:iam::515092417918:role/eric_474', 
                                           RoleSessionName='test-session',
                                           DurationSeconds=900)
-                                          
-                                          
-    aws2_dynamodb_client = boto3.client('dynamodb', region_name='us-east-1',
+                                        
+    aws2_dynamodb_resource = boto3.resource('dynamodb', region_name='us-east-1',
                                         aws_access_key_id=sts_response['Credentials']['AccessKeyId'],
                                         aws_secret_access_key=sts_response['Credentials']['SecretAccessKey'],
                                         aws_session_token=sts_response['Credentials']['SessionToken'])
                                         
-    userId = str(event['pathParameters']['id'])
-
     
-    response = aws2_dynamodb_client.scan(
-        TableName="decks",
-        FilterExpression=Attr('deckID').eq("2903928"),
+    #Get decksTable
+    decksTable = aws2_dynamodb_resource.Table('decks')
+    givenUserId = str(event['pathParameters']['id'])
+    
+    #Scan the decks table for decks that have the given User ID 
+    response = decksTable.scan(
+        FilterExpression=Attr('userid').eq(givenUserId)
     )
+    
+    data = response['Items']
+    
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        data.extend(response['Items'])
 
-    return response
+
+    return {
+        "isBase64Encoded": True,
+        "statusCode" : 200,
+        "headers": {
+                    "Access-Control-Allow-Origin" : "*", 
+                    "Access-Control-Allow-Credentials" : True 
+        },
+        "body" : json.dumps(data)
+    }
 
