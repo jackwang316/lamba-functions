bcb079809e78a0d7993897007c5208a3b1a9a7f2
Eric Reyes <43354881+ejrey@users.noreply.github.com>
2023-03-10 23:39:53 -0800

    Update getDeckByOwnerId.py
    fixed stringset error

--- getDeckByOwnerId.py
+++ getDeckByOwnerId.py
@@ -1,23 +1,12 @@
 import boto3
 import json
+import decimal
 from boto3.dynamodb.conditions import Key, Attr
 
-#For Reference on scanning the table
-#https://dynobase.dev/dynamodb-python-with-boto3/#scan
-
-sts_client = boto3.client('sts')
-
 def lambda_handler(event, context):
-    sts_response = sts_client.assume_role(RoleArn='arn:aws:iam::515092417918:role/eric_474', 
-                                          RoleSessionName='test-session',
-                                          DurationSeconds=900)
                                         
-    aws2_dynamodb_resource = boto3.resource('dynamodb', region_name='us-east-1',
-                                        aws_access_key_id=sts_response['Credentials']['AccessKeyId'],
-                                        aws_secret_access_key=sts_response['Credentials']['SecretAccessKey'],
-                                        aws_session_token=sts_response['Credentials']['SessionToken'])
+    aws2_dynamodb_resource = boto3.resource('dynamodb', region_name="us-east-1")
                                         
-    
     #Get decksTable
     decksTable = aws2_dynamodb_resource.Table('decks')
     givenUserId = str(event['pathParameters']['id'])
@@ -33,14 +22,27 @@ def lambda_handler(event, context):
         response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
         data.extend(response['Items'])
 
-
+    
     return {
         "isBase64Encoded": True,
         "statusCode" : 200,
         "headers": {
-                    "Access-Control-Allow-Origin" : "*", 
-                    "Access-Control-Allow-Credentials" : True 
-        },
-        "body" : json.dumps(data)
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin" : "*", 
+            "Access-Control-Allow-Credentials" : True ,
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+            },
+        "body" : json.dumps(data, cls=DecimalEncoder)
     }
-
+    
+    
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, set):
+            return list(o)
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
